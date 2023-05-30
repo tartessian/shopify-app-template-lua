19471a00fe8dd211dd03c1f7fe116472e9531dc4
Enrique Melero <emelero.dev@pm.me>
2023-05-30 21:48:51 +0000

    Set port provided dynamically by Shopify CLI

--- web/config.lua
+++ web/config.lua
@@ -3,7 +3,8 @@ local config = require("lapis.config")
 config("development", {
   server = "nginx",
   code_cache = "off",
-  num_workers = "1"
+  num_workers = "1",
+  port = os.getenv("PORT")
 })
 
 config("production", {
